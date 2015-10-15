661140034eb3894f6c329e88dc083bb44ca6bbc2
syhsyh9696 <sunyuanhao123456@gmail.com>
2015-10-15 21:09:07 +0800

    Add ListNode.h

---
+++ ListNode.h
@@ -0,0 +1,29 @@
+#ifndef LISTNODE_H
+#define LISTNODE_H
+
+template< typename NODETYPE >
+class ListNode
+{
+	friend class List< NODETYPE >
+public:
+	ListNode( const NODETYPE & );
+	NODETYPE getdata() const;
+private:
+	NODETYPE data;
+	ListNode< NODETYPE > *nextPtr; //next node in list 
+
+	//~ListNode();
+}; //end class ListNode
+
+template< typename NODETYPE >
+ListNode< NODETYPE >::ListNode( const NODETYPE &info )
+	:data( info ),nextPtr( 0 )
+	{}//end ListNode construtor
+
+template< typename NODETYPE >
+NODETYPE ListNode< NODETYPE >::getdata() const
+{
+	return data;
+}//end function get data
+
+#end if
