69104bfe53f415cebc4d06047047d651ff598066
GenSyhius <sunyuanhao123456@gmail.com>
2015-11-10 21:17:11 +0800

    Add new function

--- HashFunction.cpp
+++ HashFunction.cpp
@@ -1,7 +1,9 @@
 #include "iostream"
 #include "string"
 using namespce std;
-
+/**
+*Easy one
+*/
 int hash( const string &key, int tableSize ) {
 	int hashVal = 0;
 
@@ -10,6 +12,24 @@ int hash( const string &key, int tableSize ) {
 
 	return (hashVal % tableSize)
 }
+
+/**
+*A hash routine for string object
+*/
+int hash( const string &key, int tableSize )
+{
+	int hashVal = 0;
+	for ( int i = 0; i < key.lenth( ); ++i )
+		hashVal = 37 * hashVal + key[i];
+
+	hashVal %= tableSize;
+	if ( hashVal < 0 ) {
+		hashVal += tableSize;
+	}
+
+	return hashVal;
+}
+
 
 int main(int argc, char const *argv[])
 {
