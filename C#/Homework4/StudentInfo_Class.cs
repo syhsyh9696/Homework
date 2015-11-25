5c12035d56054d5260adaef011cc6ff2c67ceb58
GenSyhius <sunyuanhao123456@gmail.com>
2015-11-26 00:49:13 +0800

    New C# homework

---
+++ C#/Homework4/StudentInfo_Class.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12 {
+    class StudentInfo_Class {
+        public StudentInfo_Class(int x) {
+            _length = x;
+            _maxcapacity = x;
+            _list = new Student[x]; //?Q
+        }
+
+        public int  Maxcapacity {
+            get { return _maxcapacity; }
+        }
+        public int Currentlength {
+            get { return _length; }
+        }
+        public int Restlength {
+            get { return (_maxcapacity - _length); }
+        }
+
+        public Student this[string StudentId] {
+            get {
+                for(int i = 0; i<_maxcapacity; ++i) {
+                    if (_list[i].No == StudentId)
+                        return _list[i];
+                }
+                return null;
+            }
+
+            set {
+                for(int i = 0; i<_maxcapacity; ++i) {
+                    if (_list[i].No == StudentId) {
+                        string Tempname;
+                        string Tempclass;
+                        string Tempscore;
+
+                        Console.WriteLine("Please enter the name of the student: ");
+                        Tempname = Console.ReadLine();
+                        _list[i].Name = Tempname;
+
+                        Console.WriteLine("Please enter the class of the student: ");
+                        Tempclass = Console.ReadLine();
+                        _list[i].Class = Tempclass;
+
+                        Console.WriteLine("Please enter the score of the student: ");
+                        Tempscore = Console.ReadLine();
+                        _list[i].Score = Tempscore;
+                    }
+                }
+            }
+        } //end Index function
+        
+        public Boolean AddInfo() {
+            if (_maxcapacity == _length)
+                return false;
+            else
+                return true;
+        }
+
+        public Boolean DeleteInfo(string StudentNo) {
+            for(int i = 0; i < _maxcapacity; ++i) {
+                if (_list[i].No == StudentNo)
+                    return true;
+            }
+            return false;
+
+        }
+
+        private Student[] _list; //?Q
+        private int _maxcapacity;
+        private int _length;
+    }
+}
---
+++ C#/Homework4/Student_Class.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12 {
+    class Student {
+        public Student(string No, string Name, string Class, string Score) {
+            No = _no;
+            Name = _name;
+            Class = _class;
+            Score = _score;
+        } //end constructor
+
+        public string No {
+            set { _no = value; }
+            get { return _no; }
+        }
+        public string Name {
+            set { _name = value; }
+            get { return _name; }
+        }
+        public string Class {
+            set { _class = value; }
+            get { return _class; }
+        }
+        public string Score {
+            set { _score = value; }
+            get { return _score; }
+        }
+
+        //data 
+        private string _no;
+        private string _name;
+        private string _class;
+        private string _score;
+
+    }//end Student class
+
+}
